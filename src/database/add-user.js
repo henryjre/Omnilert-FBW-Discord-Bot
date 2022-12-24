36b35e5e3543f2bbdb98bcee29ba8ec094cc4500
henryjre <pinedahenryjre@gmail.com>
2022-12-24 23:18:33 +0800

    huge database update

---
+++ src/database/add-user.js
@@ -0,0 +1,142 @@
+const { EmbedBuilder } = require("discord.js");
+const mysql = require("mysql2/promise");
+require("dotenv").config({ path: "src/.env" });
+
+export async function addPersonalDetails(
+  fullName,
+  birthdate,
+  gender,
+  email,
+  mobileNumber,
+  address,
+  frontId,
+  backId,
+  selfieWithId,
+  referrerId
+) {
+  const connection = await mysql.createConnection({
+    host: process.env.sqlHost,
+    user: process.env.sqlUsername,
+    password: process.env.sqlPassword,
+    database: process.env.sqlDatabase,
+    port: process.env.sqlPort,
+  });
+  const id = new Date().getTime();
+
+  const memberId = `LEV${id}IOSA`;
+
+  const queryDetails =
+    "INSERT INTO Personal_Details VALUES ('" +
+    memberId +
+    "','" +
+    fullName +
+    "','" +
+    birthdate +
+    "','" +
+    gender +
+    "','" +
+    email +
+    "','" +
+    "','" +
+    mobileNumber +
+    "','" +
+    address +
+    "','" +
+    frontId +
+    "','" +
+    backId +
+    "','" +
+    selfieWithId +
+    "'";
+  await connection.query(queryDetails).catch((err) => console.log(err));
+
+  let referrer;
+  let refId;
+  if (referrerId.length != 20) {
+    refId = "none";
+    referrer = "none";
+  } else {
+    const selectQueryDetails =
+      "SELECT FULL_NAME FROM Personal_Details WHERE MEMBER_ID = '" +
+      referrerId +
+      "'";
+
+    const refQuery = await connection
+      .query(selectQueryDetails)
+      .catch((err) => console.log(err));
+
+    refId = referrerId;
+    referrer = refQuery[0][0]["FULL_NAME"];
+  }
+
+  const refQueryDetails =
+    "INSERT INTO Referral_Details VALUES ('" +
+    memberId +
+    "','" +
+    fullName +
+    "','" +
+    0 +
+    "','" +
+    refId +
+    "','" +
+    referrer +
+    "'";
+  await connection.query(refQueryDetails).catch((err) => console.log(err));
+
+  connection.end();
+
+  const date = new Date(Date.now()).toLocaleString("en-US", {
+    dateStyle: "full",
+    timeStyle: "short",
+  });
+
+  const embed = new EmbedBuilder()
+    .setTitle("✅ MEMBER CREATED")
+    .setColor("8e18be")
+    .setFooter({
+      text: date,
+    })
+    .setImage(selfieWithId)
+    .addFields([
+      {
+        name: `MEMBER ID`,
+        value: memberId,
+      },
+      {
+        name: `NAME`,
+        value: fullName,
+      },
+      {
+        name: `GENDER`,
+        value: gender,
+      },
+      {
+        name: `DATE OF BIRTH`,
+        value: birthdate,
+      },
+      {
+        name: `MOBILE NUMBER`,
+        value: mobileNumber,
+      },
+      {
+        name: `EMAIL`,
+        value: email,
+      },
+      {
+        name: `ADDRESS`,
+        value: `${address}\n\u200b`,
+      },
+      {
+        name: `REFERRER ID`,
+        value: refId,
+      },
+      {
+        name: `REFERRED BY`,
+        value: referrer,
+      },
+    ]);
+
+  await interaction.reply({
+    embeds: [embed],
+  });
+}
--- src/webhook/jotformListen.js
+++ src/webhook/jotformListen.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require('cors');   
-const helmet = require('helmet');
+const cors = require("cors");
+const helmet = require("helmet");
 
 require("dotenv").config({ path: "src/.env" });
 const chalk = require("chalk");
-const authenticateToken = require('./auth')
+const authenticateToken = require("./auth");
+const { addDatabaseDetails } = require("../database/add-user");
 
 const PORT = process.env.PORT;
 
-const app = express()
+const app = express();
 
 const listen = async () => {
   app.use(bodyParser.json());
@@ -22,16 +23,24 @@ const listen = async () => {
   });
 
   app.post("/api/registerUser", authenticateToken, async (req, res) => {
-
-    console.log(req.body)
+    await addDatabaseDetails(
+      req.body.full_name,
+      req.body.birthdate,
+      req.body.gender,
+      req.body.email_Address,
+      req.body.mobile_number,
+      req.body.address,
+      req.body.front_id,
+      req.body.back_Id,
+      req.body.selfie_image,
+      req.body.referrer_id
+    );
     res.send("🟢 Successful!");
     return;
   });
 
   app.listen(PORT, () =>
-    console.log(
-      chalk.yellow(`🟠 Webhook running on PORT ${PORT}`)
-    )
+    console.log(chalk.yellow(`🟠 Webhook running on PORT ${PORT}`))
   );
 };
 
