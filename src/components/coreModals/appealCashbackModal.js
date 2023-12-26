8b37740661edaa9166824f95b3aa7a15121ec2bd
henryjre <pinedahenryjre@gmail.com>
2023-12-27 01:08:19 +0800

    updates

--- src/components/coreModals/appealCashbackModal.js
+++ src/components/coreModals/appealCashbackModal.js
@@ -1,6 +1,6 @@
 module.exports = {
   data: {
-    name: `proposalResolutionModal`,
+    name: `appealCashbackModal`,
   },
   async execute(interaction, client) {
     await interaction.deferUpdate();
