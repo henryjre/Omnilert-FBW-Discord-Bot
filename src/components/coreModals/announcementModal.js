0a0644b974228a9afa055e8b4f114d34a0f706ff
henryjre <pinedahenryjre@gmail.com>
2024-01-30 19:42:57 +0800

    update

--- src/components/coreModals/announcementModal.js
+++ src/components/coreModals/announcementModal.js
@@ -42,7 +42,7 @@ module.exports = {
     //   .setColor("Green");
 
     const announcementEmbed = new EmbedBuilder()
-      .setDescription(`# 📢 ANNOUNCEMENT\n## *${title}*\n\u200b\n${details}`)
+      .setDescription(`# 📢 ANNOUNCEMENT\n## *${title}*\n\u200b\n${details}\n\u200b`)
       .addFields([
         {
           name: "Prepared By",
