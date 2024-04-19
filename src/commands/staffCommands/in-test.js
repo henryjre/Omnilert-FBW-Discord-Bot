99bde00106c55f99fb1bd82fb44f52fbd8552894
henryjre <pinedahenryjre@gmail.com>
2024-04-19 17:52:32 +0800

    added new reportal system

---
+++ src/commands/staffCommands/in-test.js
@@ -0,0 +1,142 @@
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  ButtonBuilder,
+  StringSelectMenuOptionBuilder,
+  StringSelectMenuBuilder,
+  ActionRowBuilder,
+  ButtonStyle,
+} = require("discord.js");
+
+const conn = require("../../sqlConnection.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("test-in")
+    .setDescription("Log in to start a task."),
+  async execute(interaction, client) {
+    await interaction.deferReply();
+    try {
+      const mgmt_connection = await conn.managementConnection();
+      const member = interaction.guild.members.cache.get(interaction.user.id);
+      const messagePayload = {};
+      try {
+        const queryTasks = `SELECT * FROM Executive_Tasks WHERE EXECUTIVE_ID = ? ORDER BY TIME_RENDERED ASC`;
+        // const queryExecutive = `SELECT * FROM Executives WHERE MEMBER_ID = ?`;
+
+        // const [executive] = await mgmt_connection.query(queryExecutive, [
+        //   member.user.id,
+        // ]);
+
+        // if (executive[0].OFFICE_ID !== interaction.channelId) {
+        //   throw new Error(
+        //     `Use this command on your office channel: <#${executive[0].OFFICE_ID}>`
+        //   );
+        // }
+
+        const [tasks] = await mgmt_connection.query(queryTasks, [
+          member.user.id,
+        ]);
+
+        const in_embed = new EmbedBuilder()
+          .setDescription(`## LOG IN \n*Add a task to start working.*`)
+          .setColor("#4F7942")
+          .setFooter({ text: `${member.nickname}` });
+
+        const addTaskButton = new ButtonBuilder()
+          .setCustomId("reportalAddTask")
+          .setLabel("Add Task")
+          .setStyle(ButtonStyle.Primary);
+
+        // const editTaskButton = new ButtonBuilder()
+        //   .setCustomId("reportalTaskEdit")
+        //   .setLabel("Edit Task")
+        //   .setStyle(ButtonStyle.Secondary);
+
+        const startButton = new ButtonBuilder()
+          .setCustomId("reportalStart")
+          .setLabel("Start")
+          .setStyle(ButtonStyle.Success)
+          .setDisabled(true);
+
+        if (tasks.length > 0) {
+          const taskOptions = [];
+          let taskEmbedDescription = "";
+          let totalHoursRendered = 0;
+          for (const task of tasks) {
+            const option = new StringSelectMenuOptionBuilder()
+              .setLabel(task.TASK_NAME)
+              .setValue(`${task.TASK_ID}_${task.TASK_NAME}`);
+
+            if (task.TASK_DESCRIPTION) {
+              option.setDescription(task.TASK_DESCRIPTION);
+            }
+
+            taskOptions.push(option);
+
+            const totalTaskTime = task.TIME_RENDERED;
+            const totalHours = Math.floor(totalTaskTime / 60);
+            const totalMinutes = totalTaskTime % 60;
+
+            taskEmbedDescription += `${task.TASK_NAME}・**\`⏱️ ${totalHours} ${
+              totalHours === 1 ? "hour" : "hours"
+            } and ${totalMinutes} ${
+              totalMinutes === 1 ? "minute" : "minutes"
+            }\`**\n`;
+
+            totalHoursRendered += task.TIME_RENDERED;
+          }
+
+          const taskMenu = new StringSelectMenuBuilder()
+            .setCustomId("reportalTasks")
+            .setPlaceholder("Select a task to do.")
+            .setMinValues(0)
+            .setMaxValues(1)
+            .addOptions(taskOptions);
+
+          const menuRow = new ActionRowBuilder().addComponents(taskMenu);
+
+          messagePayload.components = [menuRow];
+
+          const totalHours = Math.floor(totalHoursRendered / 60);
+          const totalMinutes = totalHoursRendered % 60;
+
+          const totalRenderedTime = `**\`⏱️ ${totalHours} ${
+            totalHours === 1 ? "hour" : "hours"
+          } and ${totalMinutes} ${
+            totalMinutes === 1 ? "minute" : "minutes"
+          }\`**`;
+
+          in_embed
+            .setDescription(
+              `## LOG IN \n**Total Hours Rendered**・${totalRenderedTime}\n\n**All Tasks**\n${taskEmbedDescription}`
+            )
+            .setColor("#228B22");
+        }
+
+        const buttonRow = new ActionRowBuilder().addComponents(
+          addTaskButton,
+          //   editTaskButton,
+          startButton
+        );
+
+        if (messagePayload.components) {
+          messagePayload.components.push(buttonRow);
+        } else {
+          messagePayload.components = [buttonRow];
+        }
+
+        messagePayload.embeds = [in_embed];
+
+        await interaction.editReply(messagePayload);
+      } finally {
+        mgmt_connection.destroy();
+      }
+    } catch (error) {
+      console.log(error.stack);
+      await interaction.editReply({
+        content: `🔴 ERROR: ${error.message}.`,
+      });
+    }
+  },
+};
---
+++ src/components/button/reportalButtons/startReportal.js
@@ -0,0 +1,130 @@
+const { EmbedBuilder } = require("discord.js");
+const moment = require("moment-timezone");
+
+const { customAlphabet } = require("nanoid");
+const nanoid = customAlphabet("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ", 6);
+const dbId = customAlphabet("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ", 13);
+
+const conn = require("../../../sqlConnection.js");
+
+module.exports = {
+  data: {
+    name: `reportalStart`,
+  },
+  async execute(interaction, client) {
+    try {
+      await interaction.deferUpdate();
+
+      if (interaction.user.id !== interaction.message.interaction.user.id) {
+        await interaction.followUp({
+          content: "You cannot use this button.",
+          ephemeral: true,
+        });
+        return;
+      }
+
+      const mgmt_connection = await conn.managementConnection();
+      const member = interaction.guild.members.cache.get(interaction.user.id);
+      const message = await interaction.message.channel.messages.fetch(
+        interaction.message.id
+      );
+
+      const messageEmbed = interaction.message.embeds[0];
+
+      try {
+        const timeIn = Date.now();
+
+        const getCurrentReportal =
+          "SELECT * FROM Executive_Reportals WHERE EXECUTIVE_ID = ? AND TIME_OUT_UNIX IS NULL";
+        const [currentReportal] = await mgmt_connection.query(
+          getCurrentReportal,
+          [member.user.id]
+        );
+
+        if (currentReportal.length > 0) {
+          await interaction.followUp({
+            content: `🔴 ERROR: You currently have a running shift. Please use /out to log out before logging in.`,
+            ephemeral: true,
+          });
+          return;
+        }
+
+        const timeInMoment = moment.unix(timeIn).tz("Asia/Manila");
+
+        const formattedTimestamp = timeInMoment.format("MMM D, YYYY h:mm A");
+        const nextPenaltyTimestamp = timeInMoment
+          .clone()
+          .add(30, "minutes")
+          .format("MMM D, YYYY h:mm A");
+
+        const taskName = messageEmbed.fields.find(
+          (f) => f.name === "Current Selected Task"
+        ).value;
+        const taskId = messageEmbed.fields.find(
+          (f) => f.name === "Task ID"
+        ).value;
+
+        const threadEmbed = new EmbedBuilder()
+          .setTitle(`🟢 LOG IN`)
+          .setDescription(
+            `👤 **User:** ${member.nickname}\n⏱️ **Time In:** ${formattedTimestamp}\n⏱️ **Penalty Time:** ${nextPenaltyTimestamp}`
+          )
+          .addFields([
+            {
+              name: "Current Task",
+              value: taskName,
+            },
+          ])
+          .setColor("Green");
+
+        await message.delete();
+
+        const thread = await interaction.channel.threads.create({
+          name: `${nanoid()} | ${member.nickname} - ${taskName}`,
+          autoArchiveDuration: 1440,
+        });
+        await thread.join();
+        await thread.members.add(interaction.user.id);
+
+        await interaction.channel.setName(
+          interaction.channel.name.replace("🔴", "🟢")
+        );
+
+        const embed = new EmbedBuilder()
+          .setDescription(
+            `### New Reportal Thread Created!\nYou can send your reportals to this thread channel: <#${thread.id}>`
+          )
+          .setColor("#2B2D31");
+
+        await thread.send({
+          embeds: [threadEmbed],
+        });
+
+        await interaction.followUp({
+          embeds: [embed],
+          ephemeral: true,
+        });
+
+        const insertReportal =
+          "INSERT INTO Executive_Reportals (ID, EXECUTIVE_ID, EXECUTIVE_NAME, TIME_IN_UNIX, TASK_ID, TASK_NAME, DISCORD_THREAD_ID) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        await mgmt_connection.query(insertReportal, [
+          dbId(),
+          member.user.id,
+          member.nickname,
+          timeIn,
+          taskId,
+          taskName,
+          thread.id,
+        ]);
+      } finally {
+        mgmt_connection.destroy();
+      }
+    } catch (error) {
+      console.log(error.stack);
+      await interaction.followUp({
+        content: `🔴 ERROR: ${error.message}.`,
+        ephemeral: true,
+      });
+    }
+  },
+};
