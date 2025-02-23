30518f0a7f4464f1cc15e3bd6ff4a98405b77278
henryjre <pinedahenryjre@gmail.com>
2025-02-23 20:30:20 +0800

    [UPDATE] added auto deployment to digitalocean droplet

--- src/webhook/app.js
+++ src/webhook/app.js
@@ -5,6 +5,7 @@ const app = express();
 const routes = require("./routes");
 const shopRoutes = require("./shopRoutes");
 const PORT = process.env.PORT || 3000;
+const SECRET = process.env.githubSecret;
 
 const authenticate = require("./auth");
 
@@ -14,6 +15,33 @@ app.use(express.json());
 
 app.use("/shops", shopRoutes);
 app.use("/api", authenticate, routes);
+
+app.post("/github-webhook", (req, res) => {
+  const signature = `sha256=${crypto
+    .createHmac("sha256", SECRET)
+    .update(JSON.stringify(req.body))
+    .digest("hex")}`;
+
+  const isAllowed = req.headers["x-hub-signature-256"] === signature;
+
+  if (!isAllowed) {
+    return res.status(401).send("Unauthorized");
+  }
+
+  console.log("Received push event from GitHub. Pulling latest changes...");
+
+  exec(
+    "cd /opt/discord-bot && git pull origin main && npm install && pm2 restart discord-bot",
+    (err, stdout, stderr) => {
+      if (err) {
+        console.error(`Error: ${stderr}`);
+        return res.status(500).send("Deployment failed.");
+      }
+      console.log(`Deployment output: ${stdout}`);
+      res.status(200).send("Deployed successfully!");
+    }
+  );
+});
 
 // Turn on that server!
 app.listen(PORT, () => {
