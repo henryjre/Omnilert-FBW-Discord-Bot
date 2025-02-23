750f5ac4ab2db53f504829355c6ee4e390b80fe4
henryjre <pinedahenryjre@gmail.com>
2025-02-23 22:14:29 +0800

    changed the auto deployment code to optimal

--- src/webhook/app.js
+++ src/webhook/app.js
@@ -32,17 +32,22 @@ app.post("/github-webhook", (req, res) => {
 
   console.log("Received push event from GitHub. Pulling latest changes...");
 
-  exec(
-    "cd /opt/omnilert-bot && git pull origin main && npm install && pm2 restart discord-bot",
-    (err, stdout, stderr) => {
-      if (err) {
-        console.error(`Error: ${stderr}`);
-        return res.status(500).send("Deployment failed.");
-      }
-      console.log(`Deployment output: ${stdout}`);
-      res.status(200).send("Deployed successfully!");
+  exec("git diff --name-only HEAD^ HEAD", (err, stdout) => {
+    if (err) return console.error(`Error checking changes: ${err}`);
+
+    let cmd = "cd /opt/discord-bot && git pull origin main";
+
+    if (stdout.includes("package.json")) {
+      cmd += " && npm install";
     }
-  );
+
+    cmd += " && pm2 restart discord-bot";
+
+    exec(cmd, (err, stdout, stderr) => {
+      if (err) console.error(`Error: ${stderr}`);
+      console.log(`Deployment output: ${stdout}`);
+    });
+  });
 });
 
 // Turn on that server!
