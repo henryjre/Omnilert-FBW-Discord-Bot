59066b0dfceb60653f0de3678703f2729e860c08
henryjre <pinedahenryjre@gmail.com>
2025-03-29 14:13:05 +0800

    [UPDATE] fix attachments on send

--- src/components/button/announcementButtons/announcementSubmit.js
+++ src/components/button/announcementButtons/announcementSubmit.js
@@ -48,7 +48,9 @@ module.exports = {
     const announcementMessage = await client.channels.cache.get(channel).send({
       content: targetField.value,
       embeds: interaction.message.embeds,
-      files: interaction.message.attachments,
+      files: interaction.message.attachments?.map(
+        (attachment) => attachment.url
+      ),
     });
 
     const existingThread = await interaction.channel.threads.cache.find((t) =>
