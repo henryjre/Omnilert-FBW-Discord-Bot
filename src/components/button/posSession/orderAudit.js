9ca7dde597db260ef86eac30c84111a3ddfd2591
shenon pc <pinedahenryjre@gmail.com>
2025-06-17 20:47:10 +0800

    Add order audit modal functionality in orderAudit.js, allowing users with the accounting role to submit audit opinions directly from message embeds, enhancing interaction and record-keeping.

---
+++ src/components/button/posSession/orderAudit.js
@@ -0,0 +1,88 @@
+const {
+  ActionRowBuilder,
+  MessageFlags,
+  EmbedBuilder,
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle,
+} = require("discord.js");
+
+const accountingRole = "1314413671245676685";
+
+module.exports = {
+  data: {
+    name: `posOrderAudit`,
+  },
+  async execute(interaction, client) {
+    let messageEmbed = interaction.message.embeds[0];
+
+    const replyEmbed = new EmbedBuilder();
+
+    if (!interaction.member.roles.cache.has(accountingRole)) {
+      replyEmbed
+        .setDescription(`🔴 ERROR: You cannot use this button.`)
+        .setColor("Red");
+
+      return await interaction.reply({
+        embeds: [replyEmbed],
+        flags: MessageFlags.Ephemeral,
+      });
+    }
+
+    const modal = new ModalBuilder()
+      .setCustomId(`orderAudit_${interaction.id}`)
+      .setTitle(`POS Order Audit`);
+
+    const details = new TextInputBuilder()
+      .setCustomId(`auditReason`)
+      .setLabel(`Audit Opinion`)
+      .setPlaceholder(`Insert the audit opinion here.`)
+      .setMaxLength(1000)
+      .setStyle(TextInputStyle.Paragraph)
+      .setRequired(true);
+
+    const firstRow = new ActionRowBuilder().addComponents(details);
+    modal.addComponents(firstRow);
+    await interaction.showModal(modal);
+
+    const modalResponse = await interaction.awaitModalSubmit({
+      filter: async (i) => {
+        const f =
+          i.customId === `orderAudit_${interaction.id}` &&
+          i.user.id === interaction.user.id;
+
+        if (f) {
+          await i.deferUpdate();
+        }
+        return f;
+      },
+      time: 300000,
+    });
+
+    try {
+      if (modalResponse.isModalSubmit()) {
+        const details = modalResponse.fields.getTextInputValue("auditReason");
+
+        const auditor = interaction.member.nickname.replace(/^[🔴🟢]\s*/, "");
+
+        if (details) {
+          messageEmbed.data.description += `\n\u200b\nAudit Opinion:\n> *"${details}"*\n\u200b`;
+        }
+
+        messageEmbed.data.footer = {
+          text: `${messageEmbed.data.footer.text}\n\u200b\nAudited By: ${auditor}\nAudit Opinion: *"${details}"*`,
+        };
+
+        await interaction.message.edit({
+          embeds: [messageEmbed],
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      await modalResponse.followUp({
+        content: `🔴 ERROR: An error occurred while creating your signature request. Please try again.`,
+        flags: MessageFlags.Ephemeral,
+      });
+    }
+  },
+};
