ab44a412975a77ea9c20f5806b389bcfc77523ae
shenon pc <pinedahenryjre@gmail.com>
2025-06-17 17:13:34 +0800

    Add orderDiscountProof command to handle image attachments in discount proof threads, updating original messages with the latest image. This enhances order processing functionality by ensuring visual proof is included in the thread.

---
+++ src/events/custom/discountProof.js
@@ -0,0 +1,27 @@
+const { ButtonBuilder, ButtonStyle, ActionRowBuilder } = require("discord.js");
+
+module.exports = {
+  name: "orderDiscountProof",
+  async execute(message, thread, client) {
+    // Check if message has attachments
+    if (message.attachments.size <= 0) return;
+    const mediaAttachments = message.attachments.filter((attachment) =>
+      attachment.contentType?.startsWith("image/")
+    );
+
+    if (mediaAttachments.size <= 0) return;
+
+    const latestImageAttachment = mediaAttachments.last();
+
+    const originalMessage = await thread.fetchStarterMessage();
+
+    let messageEmbed = originalMessage.embeds[0];
+
+    messageEmbed.image = { url: latestImageAttachment.url };
+
+    await originalMessage.edit({
+      embeds: [messageEmbed],
+      components: [buttonRow1, buttonRow2],
+    });
+  },
+};
