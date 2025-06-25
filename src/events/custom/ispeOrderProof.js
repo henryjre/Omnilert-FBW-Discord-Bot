a2c8695065f5c8cfccb42089e3ef26d4b32826cc
shenon pc <pinedahenryjre@gmail.com>
2025-06-25 13:50:26 +0800

    Add ispeOrderProof event handler to process image attachments in Discord messages. This implementation sends images to a specified CDN channel and updates the original message with embedded image links, enhancing receipt proof functionality.

---
+++ src/events/custom/ispeOrderProof.js
@@ -0,0 +1,53 @@
+const cdnChannel = "1384688917155938354";
+
+module.exports = {
+  name: "ispeOrderProof",
+  async execute(message, thread, client) {
+    // Check if message has attachments
+    if (message.attachments.size <= 0) return;
+    const mediaAttachments = message.attachments.filter((attachment) =>
+      attachment.contentType?.startsWith("image/")
+    );
+
+    if (mediaAttachments.size <= 0) return;
+
+    const urlsArray = mediaAttachments.map((attachment) => attachment.url);
+
+    const cdnMessage = await client.channels.cache.get(cdnChannel).send({
+      content: `Sent by ${message.author.toString()}\nTimestamp: ${message.createdAt.toLocaleString(
+        "en-US",
+        {
+          timeZone: "Asia/Manila",
+        }
+      )}`,
+      files: urlsArray,
+    });
+
+    const cdnMessageAttachment = cdnMessage.attachments;
+
+    const cdnAttachmentsUrls = cdnMessageAttachment.map(
+      (attachment) => attachment.proxyURL
+    );
+
+    const originalMessage = await thread.fetchStarterMessage();
+
+    const messageEmbed = originalMessage.embeds[0];
+
+    const newEmbeds = [messageEmbed];
+
+    for (const attachment of cdnAttachmentsUrls) {
+      const attachmentEmbed = {
+        url: "https://omnilert.odoo.com/",
+        image: {
+          url: attachment,
+        },
+      };
+
+      newEmbeds.push(attachmentEmbed);
+    }
+
+    await originalMessage.edit({
+      embeds: newEmbeds,
+    });
+  },
+};
