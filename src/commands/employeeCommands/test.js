1e87d108d325c58e20d1debd9223a084ef9fed5a
Henry Pineda <86291341+henryjre@users.noreply.github.com>
2025-06-26 23:01:46 +0800

    Delete the file at 'src/commands/employeeCommands/test.js'

--- src/commands/employeeCommands/test.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("ping")
-    .setDescription("Testing purposes!"),
-  async execute(interaction, client) {
-    interaction.reply("pong!")
-  },
-};
