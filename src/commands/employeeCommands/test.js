2faad58b8a953fbb12c716a89a4e412cb37ecd46
henryjre <pinedahenryjre@gmail.com>
2025-07-04 17:06:05 +0800

    Add test command for cash breakdown interaction in employeeCommands. This new command includes an embed for displaying cash details and buttons for confirmation and reset, enhancing user interaction during cash management processes.

---
+++ src/commands/employeeCommands/test.js
@@ -0,0 +1,71 @@
+const {
+  SlashCommandBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ActionRowBuilder,
+  EmbedBuilder,
+} = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("test")
+    .setDescription("Testing purposes!"),
+  async execute(interaction, client) {
+    const embed = new EmbedBuilder()
+      .setURL("https://omnilert.odoo.com/")
+      .setDescription("## 📝 Opening Cash Breakdown")
+      .addFields({
+        name: "Opening Cash Counted",
+        value: "₱0.00",
+      })
+      .setColor("Green")
+      .setFooter({
+        text: "Add cash breakdown details by selecting the denomination below and inputting the amount.",
+      });
+
+    const confirm = new ButtonBuilder()
+      .setCustomId("posOrderVerificationConfirm")
+      .setLabel("Confirm")
+      .setStyle(ButtonStyle.Success);
+
+    const reset = new ButtonBuilder()
+      .setCustomId("cashBreakdownReset")
+      .setLabel("Reset")
+      .setStyle(ButtonStyle.Danger);
+
+    const buttonRow = new ActionRowBuilder().addComponents(confirm, reset);
+
+    const denominations = [
+      { label: "₱1000", id: "1000" },
+      { label: "₱500", id: "500" },
+      { label: "₱200", id: "200" },
+      { label: "₱100", id: "100" },
+      { label: "₱50", id: "50" },
+      { label: "₱20", id: "20" },
+      { label: "₱10", id: "10" },
+      { label: "₱5", id: "5" },
+      { label: "₱1", id: "1" },
+    ];
+
+    const denomButtonRows = [];
+    for (let i = 0; i < denominations.length; i += 3) {
+      const row = new ActionRowBuilder();
+      row.addComponents(
+        ...denominations
+          .slice(i, i + 3)
+          .map((denom) =>
+            new ButtonBuilder()
+              .setCustomId(`cashBreakdown_${denom.id}`)
+              .setLabel(denom.label)
+              .setStyle(ButtonStyle.Primary)
+          )
+      );
+      denomButtonRows.push(row);
+    }
+
+    await interaction.reply({
+      embeds: [embed],
+      components: [...denomButtonRows, buttonRow],
+    });
+  },
+};
