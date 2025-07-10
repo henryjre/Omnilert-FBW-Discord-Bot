8910a11672413c96c4fd219081d30f88c4d0244b
henryjre <pinedahenryjre@gmail.com>
2025-07-10 19:58:24 +0800

    Add signatoriesSubmit.js to implement a command for submitting signatory requests. This feature includes user role validation, dynamic message handling, and error management, enhancing the signatory process and user experience in request management.

---
+++ src/components/button/signatoriesButtons/signatoriesSubmit.js
@@ -0,0 +1,81 @@
+const {
+  ActionRowBuilder,
+  EmbedBuilder,
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle,
+  StringSelectMenuBuilder,
+  StringSelectMenuOptionBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+} = require("discord.js");
+
+module.exports = {
+  data: {
+    name: `signatoriesSubmit`,
+  },
+  async execute(interaction, client) {
+    const replyEmbed = new EmbedBuilder();
+
+    let messageEmbed = interaction.message.embeds[0];
+    const files = interaction.message.attachments.map((a) => a.url);
+
+    if (
+      !messageEmbed.data.fields
+        .find((f) => f.name === "Prepared By")
+        .value.includes(interaction.user.id)
+    ) {
+      replyEmbed
+        .setDescription(`🔴 ERROR: You cannot use this menu.`)
+        .setColor("Red");
+
+      return await interaction.editReply({ embeds: [replyEmbed] });
+    }
+
+    const sign = new ButtonBuilder()
+      .setCustomId("signatoriesSign")
+      .setLabel("Sign")
+      .setStyle(ButtonStyle.Success);
+
+    const reject = new ButtonBuilder()
+      .setCustomId("signatoriesReject")
+      .setLabel("Reject")
+      .setStyle(ButtonStyle.Danger);
+
+    const buttonRow = new ActionRowBuilder().addComponents(sign, reject);
+
+    messageEmbed.data.footer.text = `Sign the request to approve the document or reject to return it to the author.`;
+
+    const embedFields = messageEmbed.data.fields;
+
+    const firstField = embedFields[1];
+
+    if (firstField.value.includes("To be signed ⌛")) {
+      const department = firstField.value.split(" - ");
+      const channelId = department[0];
+      const role = department[1];
+      const channel = interaction.guild.channels.cache.get(channelId);
+
+      await channel.send({
+        content: `${role}`,
+        embeds: [messageEmbed],
+        components: [buttonRow],
+        files: files,
+      });
+    } else {
+      const employee = firstField.value.replace("⌛", "").trim();
+      const channel = interaction.guild.channels.cache.get(
+        "1337029532921888840"
+      );
+
+      await channel.send({
+        content: `${employee}`,
+        embeds: [messageEmbed],
+        components: [buttonRow],
+        files: files,
+      });
+    }
+
+    await interaction.message.delete();
+  },
+};
