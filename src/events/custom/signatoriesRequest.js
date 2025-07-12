7ea3011c32f7967e0c6af4b6ffcc5292342b1945
henryjre <pinedahenryjre@gmail.com>
2025-07-12 21:26:40 +0800

    Add signatoriesRequestAttachment.js to handle image and PDF attachments in signatories requests. This new module processes attachments, sends them to designated channels, and updates original messages with the corresponding embeds, enhancing the functionality and user experience of the signatories request feature.

---
+++ src/events/custom/signatoriesRequest.js
@@ -0,0 +1,109 @@
+const { MessageFlags } = require("discord.js");
+
+const imageCdnChannel = "1384688917155938354";
+const pdfCdnChannel = "1393578039471767612";
+
+module.exports = {
+  name: "signatoriesRequestAttachment",
+  async execute(message, thread, client) {
+    // Check if message has attachments
+    if (message.attachments.size <= 0) return;
+    const mediaAttachments = message.attachments.filter((attachment) =>
+      attachment.contentType?.startsWith("image/")
+    );
+
+    const pdfAttachments = message.attachments.filter(
+      (attachment) => attachment.contentType === "application/pdf"
+    );
+
+    if (mediaAttachments.size <= 0 && pdfAttachments.size <= 0) return;
+
+    const originalMessage = await thread.fetchStarterMessage();
+
+    // Image Attachment Handling
+    if (mediaAttachments.size > 0) {
+      const loaderMessage = await message.reply({
+        content: "Adding your image/s...",
+        flags: MessageFlags.Ephemeral,
+      });
+
+      const messageEmbedsArray = originalMessage.embeds;
+
+      const urlsArray = mediaAttachments.map((attachment) => attachment.url);
+      const cdnMessage = await client.channels.cache.get(imageCdnChannel).send({
+        content: `Sent by ${message.author.toString()}\nTimestamp: ${message.createdAt.toLocaleString(
+          "en-US",
+          {
+            timeZone: "Asia/Manila",
+          }
+        )}`,
+        files: urlsArray,
+      });
+
+      const cdnMessageAttachment = cdnMessage.attachments;
+
+      const cdnAttachmentsUrls = cdnMessageAttachment.map(
+        (attachment) => attachment.proxyURL
+      );
+
+      for (const attachment of cdnAttachmentsUrls) {
+        const attachmentEmbed = {
+          url: "https://omnilert.odoo.com/",
+          image: {
+            url: attachment,
+          },
+        };
+
+        messageEmbedsArray.push(attachmentEmbed);
+      }
+
+      await originalMessage.edit({
+        embeds: messageEmbedsArray,
+      });
+
+      await loaderMessage.edit({
+        content: "Image/s added successfully.",
+      });
+
+      setTimeout(() => {
+        loaderMessage.delete();
+      }, 5000);
+    }
+
+    // PDF Attachment Handling
+    if (pdfAttachments.size > 0) {
+      const loaderMessage = await message.reply({
+        content: "Adding your document...",
+        flags: MessageFlags.Ephemeral,
+      });
+
+      const latestAttachment = pdfAttachments.first();
+
+      const cdnMessage = await client.channels.cache.get(pdfCdnChannel).send({
+        content: `Sent by ${message.author.toString()}\nTimestamp: ${message.createdAt.toLocaleString(
+          "en-US",
+          {
+            timeZone: "Asia/Manila",
+          }
+        )}`,
+        files: [latestAttachment.url],
+      });
+
+      const cdnMessageAttachment = cdnMessage.attachments.first();
+
+      const cdnAttachmentUrl = cdnMessageAttachment.url;
+
+      await originalMessage.edit({
+        files: [cdnAttachmentUrl],
+      });
+
+      await loaderMessage.edit({
+        content: "Document added successfully.",
+      });
+
+      setTimeout(() => {
+        loaderMessage.delete();
+      }, 5000);
+    }
+  },
+};
