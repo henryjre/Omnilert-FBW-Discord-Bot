e86bb9e7975550d46329994db7c38b465e59bb83
henryjre <pinedahenryjre@gmail.com>
2025-08-26 15:53:37 +0800

    Enhance employee routes by adding new endpoints for employee check-in and check-out functionality. This update introduces the `check_in` and `check_out` methods from the employeeAttendance module, improving the API's capability to handle attendance tracking.

--- src/webhook/odooRoutes/employee/index.js
+++ src/webhook/odooRoutes/employee/index.js
@@ -1,8 +1,15 @@
 const employee = require("express").Router();
-const { employeeCheckIn, employeeCheckOut } = require("./employeeAttendance");
+const {
+  employeeCheckIn,
+  employeeCheckOut,
+  check_in,
+  check_out,
+} = require("./employeeAttendance");
 
 // /odoo/employee/check_in
 employee.post("/check_in", employeeCheckIn);
 employee.post("/check_out", employeeCheckOut);
+employee.post("/employee_check_in", check_in);
+employee.post("/employee_check_out", check_out);
 
 module.exports = employee;
