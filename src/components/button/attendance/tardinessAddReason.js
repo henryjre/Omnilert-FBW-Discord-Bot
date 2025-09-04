83e2b91161b52f6cf2c36a0ddc24cc303b4ab6ac
henryjre <pinedahenryjre@gmail.com>
2025-09-04 17:14:46 +0800

    Add tardiness reason modal functionality in tardinessAddReason.js. This implementation includes user and role validation to ensure only authorized users can access the modal. It features input fields for tardiness reason and message ID, enhancing user interaction and data collection for attendance management.

---
+++ src/components/button/attendance/tardinessAddReason.js
@@ -0,0 +1,77 @@
+const {
+  ActionRowBuilder,
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle,
+  MessageFlags,
+} = require("discord.js");
+
+module.exports = {
+  data: {
+    name: `tardinessAddReason`,
+  },
+  async execute(interaction, client) {
+    const mentionedUser = interaction.message.mentions?.users?.first() || null;
+    const mentionedRole = interaction.message.mentions?.roles?.first() || null;
+
+    if (mentionedUser) {
+      const isNotMentionedUser = interaction.user.id !== mentionedUser.id;
+      if (isNotMentionedUser) {
+        return await interaction.reply({
+          content: `🔴 ERROR: You cannot use this button.`,
+          flags: MessageFlags.Ephemeral,
+        });
+      }
+    }
+
+    if (mentionedRole) {
+      const doesNotHaveRole = !interaction.member.roles.cache.has(
+        mentionedRole.id
+      );
+      if (doesNotHaveRole) {
+        return await interaction.reply({
+          content: `🔴 ERROR: You cannot use this button.`,
+          flags: MessageFlags.Ephemeral,
+        });
+      }
+    }
+
+    const messageEmbed = interaction.message.embeds[0];
+    const tardinessReasonField = messageEmbed.fields.find(
+      (field) => field.name === "Tardiness Reason"
+    );
+
+    let tardinessReasonInput = "";
+
+    if (tardinessReasonField) {
+      tardinessReasonInput = tardinessReasonField.value.split("|")[1].trim();
+    }
+
+    const modal = new ModalBuilder()
+      .setCustomId("attendanceTardinessModal")
+      .setTitle(`TARDINESS REASON`);
+
+    const firstInput = new TextInputBuilder()
+      .setCustomId(`tardinessReasonInput`)
+      .setLabel(`❓ Tardiness Reason`)
+      .setStyle(TextInputStyle.Paragraph)
+      .setMaxLength(1000)
+      .setPlaceholder("Enter the reason for tardiness.")
+      .setValue(tardinessReasonInput)
+      .setRequired(true);
+
+    const secondInput = new TextInputBuilder()
+      .setCustomId(`messageId`)
+      .setLabel(`Message ID (DO NOT CHANGE)`)
+      .setStyle(TextInputStyle.Short)
+      .setMaxLength(100)
+      .setValue(interaction.message.id)
+      .setRequired(true);
+
+    const firstActionRow = new ActionRowBuilder().addComponents(firstInput);
+    const secondActionRow = new ActionRowBuilder().addComponents(secondInput);
+
+    modal.addComponents(firstActionRow, secondActionRow);
+    await interaction.showModal(modal);
+  },
+};
