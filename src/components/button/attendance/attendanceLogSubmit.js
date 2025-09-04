0a207696c7f781d8fab552bf8725d4d6837b8c87
henryjre <pinedahenryjre@gmail.com>
2025-09-04 15:47:45 +0800

    Add attendance log submission functionality in attendanceLogSubmit.js. This implementation includes user and role validation to ensure only authorized users can submit attendance logs. It introduces buttons for approval and rejection, enhances user interaction with ephemeral replies, and provides feedback upon successful submission. This update improves the overall attendance management process in Discord.

---
+++ src/components/button/attendance/attendanceLogSubmit.js
@@ -0,0 +1,72 @@
+const {
+  ActionRowBuilder,
+  MessageFlags,
+  ButtonBuilder,
+  ButtonStyle,
+} = require("discord.js");
+
+const hrRoleId = "1314815153421680640";
+
+module.exports = {
+  data: {
+    name: `attendanceLogSubmit`,
+  },
+  async execute(interaction, client) {
+    const mentionedUser = interaction.message.mentions?.users?.first() || null;
+    const mentionedRole = interaction.message.mentions?.roles?.first() || null;
+
+    if (mentionedUser) {
+      const isNotMentionedUser = interaction.user.id !== mentionedUser.id;
+      if (isNotMentionedUser) {
+        return await interaction.reply({
+          content: `🔴 ERROR: You cannot use this button.`,
+          flags: MessageFlags.Ephemeral,
+        });
+      }
+    }
+
+    if (mentionedRole) {
+      const doesNotHaveRole = !interaction.member.roles.cache.has(
+        mentionedRole.id
+      );
+      if (doesNotHaveRole) {
+        return await interaction.reply({
+          content: `🔴 ERROR: You cannot use this button.`,
+          flags: MessageFlags.Ephemeral,
+        });
+      }
+    }
+
+    await interaction.deferReply({ flags: MessageFlags.Ephemeral });
+
+    const messageEmbed = interaction.message.embeds[0];
+
+    const approve = new ButtonBuilder()
+      .setCustomId("attendanceLogApprove")
+      .setLabel("Approve")
+      .setStyle(ButtonStyle.Success);
+
+    const reject = new ButtonBuilder()
+      .setCustomId("attendanceLogReject")
+      .setLabel("Reject")
+      .setStyle(ButtonStyle.Danger);
+
+    const buttonRow = new ActionRowBuilder().addComponents(approve, reject);
+
+    await interaction.channel.send({
+      //   content: `<@&${hrRoleId}>`,
+      embeds: [messageEmbed],
+      components: [buttonRow],
+    });
+
+    await interaction.message.delete();
+
+    const replyEmbed = new EmbedBuilder()
+      .setDescription("✅ Form submitted successfully.")
+      .setColor("Green");
+
+    await interaction.editReply({
+      embeds: [replyEmbed],
+    });
+  },
+};
