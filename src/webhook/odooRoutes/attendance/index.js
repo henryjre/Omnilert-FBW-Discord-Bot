8021daac89b9ef36deb24d128de5f366d5944a06
henryjre <pinedahenryjre@gmail.com>
2025-09-13 17:17:06 +0800

    Update import path for publishedShift in attendance route. This change reflects the new file structure by importing publishedShift from planningShift.js instead of the previous location, ensuring proper functionality of the attendance management integration within the Odoo API.

--- src/webhook/odooRoutes/attendance/index.js
+++ src/webhook/odooRoutes/attendance/index.js
@@ -1,5 +1,5 @@
 const attendance = require("express").Router();
-const { publishedShift } = require("./publishedShift");
+const { publishedShift } = require("./planningShift");
 
 // /odoo/employee/check_in
 attendance.post("/published_shift", publishedShift);
