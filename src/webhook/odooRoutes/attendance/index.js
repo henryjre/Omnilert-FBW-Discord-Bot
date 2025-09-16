df5c2b2aee4b8558c7a3c84a99ebbfa045a582c5
henryjre <pinedahenryjre@gmail.com>
2025-09-16 18:24:45 +0800

    Add check-in route to attendance.js for employee attendance tracking. This update introduces the /check_in endpoint, integrating the attendanceCheckIn function to enhance the attendance management capabilities within the Odoo API.

--- src/webhook/odooRoutes/attendance/index.js
+++ src/webhook/odooRoutes/attendance/index.js
@@ -1,7 +1,9 @@
 const attendance = require("express").Router();
 const { publishedShift } = require("./planningShift");
+const { attendanceCheckIn } = require("./attendance");
 
 // /odoo/employee/check_in
 attendance.post("/published_shift", publishedShift);
+attendance.post("/check_in", attendanceCheckIn);
 
 module.exports = attendance;
