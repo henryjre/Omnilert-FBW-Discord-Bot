6babcc71480126484100020a8709a53df5c4c329
henryjre <pinedahenryjre@gmail.com>
2025-09-16 18:41:01 +0800

    Add /check_out endpoint to attendance routes for employee check-out processing. This update integrates the attendanceCheckOut function, enhancing attendance management capabilities within the Odoo API.

--- src/webhook/odooRoutes/attendance/index.js
+++ src/webhook/odooRoutes/attendance/index.js
@@ -1,9 +1,10 @@
 const attendance = require("express").Router();
 const { publishedShift } = require("./planningShift");
-const { attendanceCheckIn } = require("./attendance");
+const { attendanceCheckIn, attendanceCheckOut } = require("./attendance");
 
 // /odoo/employee/check_in
 attendance.post("/published_shift", publishedShift);
 attendance.post("/check_in", attendanceCheckIn);
+attendance.post("/check_out", attendanceCheckOut);
 
 module.exports = attendance;
