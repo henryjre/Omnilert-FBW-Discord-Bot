40bb15660b3f0b5980bc2aa591b03b6a16c98c10
henryjre <pinedahenryjre@gmail.com>
2025-09-20 14:02:46 +0800

    Add checkoutReasonButton component for managing checkout reasons in attendance. This new feature includes user role validation, modal input for checkout reasons, and ensures proper interaction handling, enhancing the attendance management system within the Odoo API.

---
+++ src/components/button/attendance/checkoutReasonButton.js
@@ -0,0 +1,77 @@
+const {
+  ActionRowBuilder,
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle,
+  MessageFlags,
+} = require("discord.js");
+
+module.exports = {
+  data: {
+    name: `tardinessAddReason`,
+  },
+  async execute(interaction, client) {
+    const mentionedUser = interaction.message.mentions?.users?.first() || null;
+    const mentionedRole = interaction.message.mentions?.roles?.first() || null;
+
+    if (mentionedUser) {
+      const isNotMentionedUser = interaction.user.id !== mentionedUser.id;
+      if (isNotMentionedUser) {
+        return await interaction.reply({
+          content: `🔴 ERROR: You cannot use this button.`,
+          flags: MessageFlags.Ephemeral,
+        });
+      }
+    }
+
+    if (mentionedRole) {
+      const doesNotHaveRole = !interaction.member.roles.cache.has(
+        mentionedRole.id
+      );
+      if (doesNotHaveRole) {
+        return await interaction.reply({
+          content: `🔴 ERROR: You cannot use this button.`,
+          flags: MessageFlags.Ephemeral,
+        });
+      }
+    }
+
+    const messageEmbed = interaction.message.embeds[0];
+    const checkoutReasonField = messageEmbed.fields.find(
+      (field) => field.name === "Reason for Checkout"
+    );
+
+    let checkoutReasonInput = "";
+
+    if (checkoutReasonField) {
+      checkoutReasonInput = checkoutReasonField.value.split("|")[1].trim();
+    }
+
+    const modal = new ModalBuilder()
+      .setCustomId("checkoutReasonModal")
+      .setTitle(`Add CheckoutReason`);
+
+    const firstInput = new TextInputBuilder()
+      .setCustomId(`checkoutReasonInput`)
+      .setLabel(`❓ Reason for Checkout`)
+      .setStyle(TextInputStyle.Paragraph)
+      .setMaxLength(1000)
+      .setPlaceholder("Enter the reason.")
+      .setValue(checkoutReasonInput)
+      .setRequired(true);
+
+    const secondInput = new TextInputBuilder()
+      .setCustomId(`messageId`)
+      .setLabel(`Message ID (DO NOT CHANGE)`)
+      .setStyle(TextInputStyle.Short)
+      .setMaxLength(100)
+      .setValue(interaction.message.id)
+      .setRequired(true);
+
+    const firstActionRow = new ActionRowBuilder().addComponents(firstInput);
+    const secondActionRow = new ActionRowBuilder().addComponents(secondInput);
+
+    modal.addComponents(firstActionRow, secondActionRow);
+    await interaction.showModal(modal);
+  },
+};
