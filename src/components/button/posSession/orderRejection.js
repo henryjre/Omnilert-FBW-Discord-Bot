571b1e3755ec43204eb2f1b7ba7e2b5f3f530be3
henryjre <pinedahenryjre@gmail.com>
2025-09-30 20:59:57 +0800

    Refactor order rejection process to replace audit button with a StringSelectMenu for user feedback. This change enhances user interaction by allowing a detailed audit rating selection through a dropdown menu, improving the overall user experience.

--- src/components/button/posSession/orderRejection.js
+++ src/components/button/posSession/orderRejection.js
@@ -7,6 +7,8 @@ const {
   TextInputStyle,
   ButtonBuilder,
   ButtonStyle,
+  StringSelectMenuBuilder,
+  StringSelectMenuOptionBuilder,
 } = require("discord.js");
 
 const departments = require("../../../config/departments.json");
@@ -130,17 +132,39 @@ module.exports = {
           )}`,
         };
 
-        const submit = new ButtonBuilder()
-          .setCustomId("posOrderAudit")
-          .setLabel("Audit")
-          .setStyle(ButtonStyle.Primary);
+        const auditRatingMenu = new StringSelectMenuBuilder()
+          .setCustomId("posAuditRatingMenu")
+          .setOptions([
+            new StringSelectMenuOptionBuilder().setLabel("⭐").setValue("⭐"),
+            new StringSelectMenuOptionBuilder()
+              .setLabel("⭐⭐")
+              .setValue("⭐⭐"),
+            new StringSelectMenuOptionBuilder()
+              .setLabel("⭐⭐⭐")
+              .setValue("⭐⭐⭐"),
+            new StringSelectMenuOptionBuilder()
+              .setLabel("⭐⭐⭐⭐")
+              .setValue("⭐⭐⭐⭐"),
+            new StringSelectMenuOptionBuilder()
+              .setLabel("⭐⭐⭐⭐⭐")
+              .setValue("⭐⭐⭐⭐⭐"),
+          ])
+          .setMinValues(1)
+          .setMaxValues(1)
+          .setPlaceholder("Select audit rating.");
 
-        const buttonRow = new ActionRowBuilder().addComponents(submit);
+        // const submit = new ButtonBuilder()
+        //   .setCustomId("posOrderAudit")
+        //   .setLabel("Audit")
+        //   .setStyle(ButtonStyle.Primary);
+
+        const menuRow = new ActionRowBuilder().addComponents(auditRatingMenu);
+        // const buttonRow = new ActionRowBuilder().addComponents(submit);
 
         await posThread.send({
           content: `<@&${managementRole}>`,
           embeds: allEmbeds,
-          components: [buttonRow],
+          components: [menuRow],
         });
 
         try {
