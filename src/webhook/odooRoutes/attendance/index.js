595f1f7dab17000cd1a05df7f31a8c533dfcad3c
henryjre <pinedahenryjre@gmail.com>
2025-09-30 16:24:21 +0800

    Add deleted shift route to attendance API. This update introduces a new POST endpoint for handling deleted shifts, enhancing the attendance management system's functionality by allowing the processing of deleted planning shifts alongside existing check-in and check-out routes.

--- src/webhook/odooRoutes/attendance/index.js
+++ src/webhook/odooRoutes/attendance/index.js
@@ -1,9 +1,10 @@
 const attendance = require("express").Router();
-const { publishedShift } = require("./planningShift");
+const { publishedShift, deletedPlanningShift } = require("./planningShift");
 const { attendanceCheckIn, attendanceCheckOut } = require("./attendance");
 
 // /odoo/employee/check_in
 attendance.post("/published_shift", publishedShift);
+attendance.post("/deleted_shift", deletedPlanningShift);
 attendance.post("/check_in", attendanceCheckIn);
 attendance.post("/check_out", attendanceCheckOut);
 
