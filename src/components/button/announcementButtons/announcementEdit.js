8c7947931f3b47aa96afac456bc55398729a3dd5
henryjre <pinedahenryjre@gmail.com>
2025-10-03 11:54:27 +0800

    Add announcementEdit.js to implement editing functionality for existing announcements. This module allows users to edit announcements through a modal interface, ensuring only the original creator can make changes, thereby enhancing user interaction and maintaining content integrity.

---
+++ src/components/button/announcementButtons/announcementEdit.js
@@ -0,0 +1,62 @@
+const {
+  EmbedBuilder,
+  MessageFlags,
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle,
+  ActionRowBuilder,
+} = require("discord.js");
+
+module.exports = {
+  data: {
+    name: `announcementEdit`,
+  },
+  async execute(interaction, client) {
+    await interaction.deferReply({ flags: MessageFlags.Ephemeral });
+    const messageEmbed = interaction.message.embeds[0];
+
+    const ownerField = messageEmbed.data.fields.find(
+      (f) => f.name === "Prepared By"
+    );
+
+    if (!ownerField.value.includes(interaction.user.id)) {
+      const replyEmbed = new EmbedBuilder()
+        .setDescription(`🔴 ERROR: You cannot use this button.`)
+        .setColor("Red");
+
+      return await interaction.editReply({
+        embeds: [replyEmbed],
+      });
+    }
+
+    console.log(messageEmbed.data.description);
+
+    return;
+
+    const modal = new ModalBuilder().setCustomId("announcementEditModal");
+
+    modal.setTitle(`Edit the announcement`);
+
+    const firstInput = new TextInputBuilder()
+      .setCustomId(`titleInput`)
+      .setLabel(`Title`)
+      .setStyle(TextInputStyle.Short)
+      .setPlaceholder("The title of your announcement")
+      .setRequired(true);
+
+    const secondInput = new TextInputBuilder()
+      .setCustomId(`announcementInput`)
+      .setLabel(`Announcement Details`)
+      .setStyle(TextInputStyle.Paragraph)
+      .setPlaceholder("The details of your announcement.")
+      .setMaxLength(4000)
+      .setRequired(true);
+
+    const firstActionRow = new ActionRowBuilder().addComponents(firstInput);
+    const secondActionRow = new ActionRowBuilder().addComponents(secondInput);
+
+    modal.addComponents(firstActionRow, secondActionRow);
+
+    await interaction.showModal(modal);
+  },
+};
