42481dfb97e3fbbba363f065fd6b496c476e8b11
henryjre <pinedahenryjre@gmail.com>
2025-10-10 00:36:25 +0800

    Add loyaltyCard.js to implement external route for retrieving loyalty card data. This new module allows fetching loyalty points based on partner ID, enhancing the API's capability for managing loyalty program interactions.

---
+++ src/webhook/odooRoutes/external/loyaltyCard.js
@@ -0,0 +1,70 @@
+const { jsonRpc } = require("../../../odooRpc.js");
+
+const dbName = process.env.odoo_db;
+const password = process.env.odoo_password;
+
+//
+// GETTING LOYALTY CARD DATA
+//
+
+const getLoyaltyCardData = async (req, res) => {
+  const { partner_id } = req.query;
+
+  try {
+    if (!partner_id) {
+      throw new Error("no_barcode_found");
+    }
+
+    const params = {
+      model: "loyalty.card",
+      method: "search_read",
+      domain: [
+        ["partner_id", "=", partner_id],
+        ["program_id", "=", "Reward Card"],
+        // ["company_id", "in", [branch.cid]],
+      ],
+      fields: ["points"],
+      offset: null,
+      limit: null,
+      //   order: "date asc",
+    };
+
+    const request = await jsonRpc("call", {
+      service: "object",
+      method: "execute",
+      args: [
+        dbName,
+        2,
+        password,
+        params.model,
+        params.method,
+        params.domain,
+        params.fields,
+        params.offset,
+        params.limit,
+        // params.order,
+      ],
+    });
+
+    if (request.error) {
+      throw new Error("rpc_error");
+    }
+
+    if (!request.result.length) {
+      throw new Error("no_data_found");
+    }
+
+    return res
+      .status(200)
+      .json({ ok: true, message: "success", data: request.result });
+  } catch (error) {
+    console.error("Error:", error);
+    return res
+      .status(404)
+      .json({ ok: false, message: error.message, data: [] });
+  }
+};
+
+module.exports = {
+  getLoyaltyCardData,
+};
