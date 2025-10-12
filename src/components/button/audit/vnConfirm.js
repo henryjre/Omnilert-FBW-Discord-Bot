c9b4db81857a6761124e0083b2ebaa02a9f87cb4
henryjre <pinedahenryjre@gmail.com>
2025-10-12 12:08:11 +0800

    Add vnConfirm.js component to handle violation notice confirmations. This feature allows users to confirm VNs, create associated threads, and update audit statuses, enhancing the audit process and user interaction with buttons for issuing or rejecting VNs.

---
+++ src/components/button/audit/vnConfirm.js
@@ -0,0 +1,171 @@
+const {
+  ActionRowBuilder,
+  MessageFlags,
+  ChannelType,
+  ButtonBuilder,
+  ButtonStyle,
+  EmbedBuilder,
+} = require("discord.js");
+
+const vnInquiriesAndDiscussionsChannelId = "1424951056558854144";
+const auditCompletedChannelId = "1423597979604095046";
+
+module.exports = {
+  data: {
+    name: `vnrConfirm`,
+  },
+  async execute(interaction, client) {
+    const mentionedUser = interaction.message.mentions?.users?.first() || null;
+    const mentionedRole = interaction.message.mentions?.roles?.first() || null;
+
+    if (mentionedUser) {
+      const isNotMentionedUser = interaction.user.id !== mentionedUser.id;
+      if (isNotMentionedUser) {
+        return await interaction.reply({
+          content: `🔴 ERROR: You cannot use this button.`,
+          flags: MessageFlags.Ephemeral,
+        });
+      }
+    }
+
+    if (mentionedRole) {
+      const doesNotHaveRole = !interaction.member.roles.cache.has(
+        mentionedRole.id
+      );
+      if (doesNotHaveRole) {
+        return await interaction.reply({
+          content: `🔴 ERROR: You cannot use this button.`,
+          flags: MessageFlags.Ephemeral,
+        });
+      }
+    }
+
+    await interaction.deferReply({ flags: MessageFlags.Ephemeral });
+
+    const confirmedBy =
+      interaction.member.nickname.replace(/^[🔴🟢]\s*/, "") ||
+      interaction.user.username;
+
+    const allEmbeds = interaction.message.embeds;
+    const messageEmbed = allEmbeds[0];
+
+    const auditCompletedChannel = await client.channels.cache.get(
+      auditCompletedChannelId
+    );
+    const auditMessageIdField = messageEmbed.data.fields.find(
+      (f) => f.name === "Audit Message ID"
+    );
+    const auditMessageId = auditMessageIdField.value;
+
+    const auditMessage = await auditCompletedChannel.messages.fetch(
+      auditMessageId
+    );
+    const auditMessageEmbed = auditMessage.embeds[0];
+
+    const auditTitle = auditMessageEmbed.data.description;
+    const auditIdMatch = auditTitle.match(/AUD-\d+/);
+
+    const vnrTitle = messageEmbed.data.description;
+    const vnrIdMatch = vnrTitle.match(/VN-\d+/);
+
+    const auditId = auditIdMatch ? auditIdMatch[0] : "0000";
+    const vnrId = vnrIdMatch ? vnrIdMatch[0] : "0000";
+
+    const vnInquiriesAndDiscussionsChannel = await client.channels.cache.get(
+      vnInquiriesAndDiscussionsChannelId
+    );
+
+    const thread = await vnInquiriesAndDiscussionsChannel.threads.create({
+      name: `Violation Notice - ${vnrId} | AUD-${auditId}`,
+      type: ChannelType.PublicThread,
+    });
+
+    const newVnEmbed = EmbedBuilder.from(messageEmbed)
+      .setDescription(`## 🚫 VIOLATION NOTICE | VN-${vnrId}`)
+      .setFooter({
+        text: `Confirmed By: ${confirmedBy}`,
+      });
+
+    const issueVnrButton = new ButtonBuilder()
+      .setCustomId("vnrIssue")
+      .setLabel("Issue VN")
+      .setStyle(ButtonStyle.Success);
+
+    const rejectVnrButton = new ButtonBuilder()
+      .setCustomId("vnrReject")
+      .setLabel("Reject VN")
+      .setStyle(ButtonStyle.Danger);
+
+    const issueVnrButtonRow = new ActionRowBuilder().addComponents(
+      issueVnrButton,
+      rejectVnrButton
+    );
+
+    const vnrThreadMessage = await thread.send({
+      embeds: [newVnEmbed],
+      components: [issueVnrButtonRow],
+    });
+
+    await editVnrStatus(
+      messageEmbed,
+      "Confirmed",
+      vnrThreadMessage.url,
+      client
+    );
+
+    const replyEmbed = new EmbedBuilder()
+      .setDescription(
+        `✅ VN has been confirmed. Please proceed to <#${vnInquiriesAndDiscussionsChannelId}> to issue the VN.`
+      )
+      .setColor("Green");
+
+    await interaction.editReply({
+      embeds: [replyEmbed],
+    });
+  },
+};
+
+async function editVnrStatus(messageEmbed, status, link, client) {
+  const auditMessageIdField = messageEmbed.data.fields.find(
+    (f) => f.name === "Audit Message ID"
+  );
+  const auditMessageId = auditMessageIdField.value;
+
+  const auditCompletedChannel = client.channels.cache.get(
+    auditCompletedChannelId
+  );
+
+  const auditMessage = await auditCompletedChannel.messages.fetch(
+    auditMessageId
+  );
+
+  const auditMessageEmbed = auditMessage.embeds[0];
+
+  const vnrStatusField = auditMessageEmbed.data.fields.find(
+    (f) => f.name === "Violation Notice Status"
+  );
+  const vnrLinkField = auditMessageEmbed.data.fields.find(
+    (f) => f.name === "Violation Notice Link"
+  );
+
+  if (vnrStatusField) {
+    vnrStatusField.value = status;
+  } else {
+    auditMessageEmbed.data.fields.push({
+      name: "Violation Notice Status",
+      value: status,
+    });
+  }
+
+  if (vnrLinkField) {
+    vnrLinkField.value = link || "No VN link found.";
+  } else {
+    auditMessageEmbed.data.fields.push({
+      name: "Violation Notice Link",
+      value: link || "No VN link found.",
+    });
+  }
+
+  await auditMessage.edit({ embeds: [auditMessageEmbed] });
+  return;
+}
