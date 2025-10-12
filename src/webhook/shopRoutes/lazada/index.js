ef4875481b98d2c0dc2fbcdc9dbb6b1fdcbeb1bb
henryjre <pinedahenryjre@gmail.com>
2025-10-12 12:26:45 +0800

    Remove shopRoutes and associated webhook handling for Lazada and Shopee. This cleanup eliminates unused routes and webhook logic, streamlining the codebase and improving maintainability.

--- src/webhook/shopRoutes/lazada/index.js
+++
@@ -1,6 +0,0 @@
-const lazada = require("express").Router();
-const webhook = require("./lazadaWebook");
-
-lazada.post("/webhook", webhook);
-
-module.exports = lazada;
--- src/webhook/shopRoutes/shopee/index.js
+++
@@ -1,6 +0,0 @@
-const shopee = require("express").Router();
-const webhook = require("./shopeeWebhook");
-
-shopee.post("/webhook", webhook);
-
-module.exports = shopee;
