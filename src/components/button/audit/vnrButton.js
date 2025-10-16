ac9854b6b4c9a66ba48fb70e2ddda6e8365b3c6f
henryjre <pinedahenryjre@gmail.com>
2025-10-16 18:51:28 +0800

    Refactor vnrButton.js to improve code consistency and readability. Changes include standardizing string quotes, commenting out user and role validation logic for future implementation, and enhancing modal component setup for better user interaction in the audit process.

--- src/components/button/audit/vnrButton.js
+++ src/components/button/audit/vnrButton.js
@@ -3,53 +3,51 @@ const {
   MessageFlags,
   ModalBuilder,
   TextInputBuilder,
-  TextInputStyle,
-} = require("discord.js");
+  TextInputStyle
+} = require('discord.js');
 
 module.exports = {
   data: {
-    name: `auditVnr`,
+    name: `auditVnr`
   },
   async execute(interaction, client) {
-    const mentionedUser = interaction.message.mentions?.users?.first() || null;
-    const mentionedRole = interaction.message.mentions?.roles?.first() || null;
+    // const mentionedUser = interaction.message.mentions?.users?.first() || null;
+    // const mentionedRole = interaction.message.mentions?.roles?.first() || null;
 
-    if (mentionedUser) {
-      const isNotMentionedUser = interaction.user.id !== mentionedUser.id;
-      if (isNotMentionedUser) {
-        return await interaction.reply({
-          content: `🔴 ERROR: You cannot use this button.`,
-          flags: MessageFlags.Ephemeral,
-        });
-      }
-    }
+    // if (mentionedUser) {
+    //   const isNotMentionedUser = interaction.user.id !== mentionedUser.id;
+    //   if (isNotMentionedUser) {
+    //     return await interaction.reply({
+    //       content: `🔴 ERROR: You cannot use this button.`,
+    //       flags: MessageFlags.Ephemeral,
+    //     });
+    //   }
+    // }
 
-    if (mentionedRole) {
-      const doesNotHaveRole = !interaction.member.roles.cache.has(
-        mentionedRole.id
-      );
-      if (doesNotHaveRole) {
-        return await interaction.reply({
-          content: `🔴 ERROR: You cannot use this button.`,
-          flags: MessageFlags.Ephemeral,
-        });
-      }
-    }
+    // if (mentionedRole) {
+    //   const doesNotHaveRole = !interaction.member.roles.cache.has(
+    //     mentionedRole.id
+    //   );
+    //   if (doesNotHaveRole) {
+    //     return await interaction.reply({
+    //       content: `🔴 ERROR: You cannot use this button.`,
+    //       flags: MessageFlags.Ephemeral,
+    //     });
+    //   }
+    // }
 
-    const modal = new ModalBuilder()
-      .setCustomId("vnrModal")
-      .setTitle("Violation Notice Request");
+    const modal = new ModalBuilder().setCustomId('vnrModal').setTitle('Violation Notice Request');
 
     const firstInput = new TextInputBuilder()
-      .setCustomId("vnrDescriptionInput")
+      .setCustomId('vnrDescriptionInput')
       .setLabel(`Brief Description`)
       .setStyle(TextInputStyle.Paragraph)
-      .setPlaceholder("Explain the description of the violation.")
+      .setPlaceholder('Explain the description of the violation.')
       .setMaxLength(1000)
       .setRequired(true);
 
     const secondInput = new TextInputBuilder()
-      .setCustomId("messageIdInput")
+      .setCustomId('messageIdInput')
       .setLabel(`Message ID (DO NOT CHANGE)`)
       .setStyle(TextInputStyle.Short)
       .setMaxLength(100)
@@ -62,5 +60,5 @@ module.exports = {
     modal.addComponents(firstActionRow, secondActionRow);
 
     await interaction.showModal(modal);
-  },
+  }
 };
