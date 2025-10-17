3bd39dd105d638f013e423a1ab04500508f8bdf3
henryjre <pinedahenryjre@gmail.com>
2025-10-17 13:25:17 +0800

    Update getNextAuditId function in sqliteFunctions.js to initialize last_id with 1 instead of 0, ensuring correct audit ID generation. This change improves the logic for handling audit ID increments during database transactions.

--- src/sqliteFunctions.js
+++ src/sqliteFunctions.js
@@ -2,11 +2,12 @@ const db = require('./sqliteConnection');
 
 const getNextAuditId = db.transaction((code) => {
   const upsertCounter = db.prepare(`
-        INSERT INTO audit_id_count(code, last_id)
-        VALUES (?, 0)
-        ON CONFLICT(code) DO UPDATE SET last_id = last_id + 1
-        RETURNING last_id;
-      `);
+    INSERT INTO audit_id_count(code, last_id)
+    VALUES (?, 1)                                   
+    ON CONFLICT(code) DO UPDATE
+      SET last_id = audit_id_count.last_id + 1      
+    RETURNING last_id;                            
+  `);
 
   const row = upsertCounter.get(code);
   return row.last_id;
