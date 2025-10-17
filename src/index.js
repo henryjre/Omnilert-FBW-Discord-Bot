b217192bab85eddd7cdc26332c796737c64e866d
henryjre <pinedahenryjre@gmail.com>
2025-10-17 12:53:36 +0800

    Refactor index.js to improve code consistency and readability. Changes include standardizing string quotes, filtering out 'code' folder from function directories, and enhancing overall structure for better maintainability.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-const {
-  Client,
-  Collection,
-  GatewayIntentBits,
-  Partials,
-} = require("discord.js");
-const fs = require("fs");
+const { Client, Collection, GatewayIntentBits, Partials } = require('discord.js');
+const fs = require('fs');
 
-require("dotenv").config({ path: "src/.env" });
+require('dotenv').config({ path: 'src/.env' });
 
 const client = new Client({
   intents: [
@@ -17,9 +12,9 @@ const client = new Client({
     GatewayIntentBits.GuildMessageReactions,
     GatewayIntentBits.GuildIntegrations,
     GatewayIntentBits.GuildVoiceStates,
-    GatewayIntentBits.GuildScheduledEvents,
+    GatewayIntentBits.GuildScheduledEvents
   ],
-  partials: [Partials.Channel, Partials.Message, Partials.Reaction],
+  partials: [Partials.Channel, Partials.Message, Partials.Reaction]
 });
 
 client.commands = new Collection();
@@ -30,28 +25,27 @@ client.modals = new Collection();
 client.cooldowns = new Collection();
 client.commandArray = [];
 
-const functionFolders = fs.readdirSync(`./src/functions`);
+const functionFolders = fs.readdirSync(`./src/functions`).filter((folder) => folder !== 'code');
 for (const folder of functionFolders) {
   const functionFiles = fs
     .readdirSync(`./src/functions/${folder}`)
-    .filter((file) => file.endsWith(".js"));
-  for (const file of functionFiles)
-    require(`./functions/${folder}/${file}`)(client);
+    .filter((file) => file.endsWith('.js'));
+  for (const file of functionFiles) require(`./functions/${folder}/${file}`)(client);
 }
 
 client.handleEvents();
 client.handleCommands();
 client.handleComponents();
 
-if (process.env.node_env === "prod") {
+if (process.env.node_env === 'prod') {
   client.login(process.env.prodToken);
-} else if (process.env.node_env === "test") {
+} else if (process.env.node_env === 'test') {
   client.login(process.env.testToken);
 }
 
 module.exports = client;
 
-if (process.env.node_env === "prod") {
-  require("./webhook/app.js");
+if (process.env.node_env === 'prod') {
+  require('./webhook/app.js');
 }
-require("./sqliteConnection.js");
+require('./sqliteConnection.js');
