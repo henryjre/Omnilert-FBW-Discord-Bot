74bc9f4bee2245e7eebc3a58ea10a20134c5e1b8
henryjre <pinedahenryjre@gmail.com>
2025-10-17 12:54:33 +0800

    Add sqliteFunctions.js to implement getNextAuditId function for managing audit IDs. This function handles the insertion and updating of audit ID counts in the database, enhancing the audit process and ensuring unique identifiers for audits.

---
+++ src/sqliteFunctions.js
@@ -0,0 +1,21 @@
+const db = require('./sqliteConnection');
+
+const getNextAuditId = db.transaction((code) => {
+  const upsertCounter = db.prepare(`
+        INSERT INTO audit_id_count(code, last_id)
+        VALUES (?, 0)
+        ON CONFLICT(code) DO UPDATE SET last_id = last_id + 1
+        RETURNING last_id;
+      `);
+
+  const insertAudit = db.prepare(`
+        INSERT INTO audits(code, audit_id) VALUES (?, ?);
+      `);
+
+  const row = upsertCounter.get(code);
+  return row.last_id;
+});
+
+module.exports = {
+  getNextAuditId
+};
