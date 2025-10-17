a8ea758a2848d598ac46dd40e6dfa14babd2a4a8
henryjre <pinedahenryjre@gmail.com>
2025-10-17 18:47:14 +0800

    Add audit route to odooRoutes and standardize string quotes

--- src/webhook/odooRoutes/index.js
+++ src/webhook/odooRoutes/index.js
@@ -1,28 +1,32 @@
-const odooRoutes = require("express").Router();
-const employee = require("./employee");
-const inventory = require("./inventory");
-const pos = require("./pos");
-const attendance = require("./attendance");
-const external = require("./external");
+const odooRoutes = require('express').Router();
+const employee = require('./employee');
+const inventory = require('./inventory');
+const pos = require('./pos');
+const attendance = require('./attendance');
+const external = require('./external');
+const audit = require('./audit');
+
+// /odoo/audit
+odooRoutes.use('/audit', audit);
 
 // /odoo/employee
-odooRoutes.use("/employee", employee);
+odooRoutes.use('/employee', employee);
 
 // /odoo/inventory
-odooRoutes.use("/inventory", inventory);
+odooRoutes.use('/inventory', inventory);
 
 // /odoo/pos
-odooRoutes.use("/pos", pos);
+odooRoutes.use('/pos', pos);
 
 // /odoo/attendance
-odooRoutes.use("/attendance", attendance);
+odooRoutes.use('/attendance', attendance);
 
 // /odoo/external
-odooRoutes.use("/external", external);
+odooRoutes.use('/external', external);
 
 // /odoo/
-odooRoutes.get("/", (req, res) => {
-  res.status(200).json({ message: "This is the Omniler Odoo API" });
+odooRoutes.get('/', (req, res) => {
+  res.status(200).json({ message: 'This is the Omniler Odoo API' });
 });
 
 module.exports = odooRoutes;
