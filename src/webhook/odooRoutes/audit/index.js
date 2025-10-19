aa22f624f76797c8918375bb8650c7b67e65af5f
henryjre <pinedahenryjre@gmail.com>
2025-10-19 21:44:37 +0800

    Add route for calculating weekly merit in audit index. This update integrates the calculateWeeklyMerit function, enhancing the audit capabilities by allowing weekly merit calculations through a new endpoint.

--- src/webhook/odooRoutes/audit/index.js
+++ src/webhook/odooRoutes/audit/index.js
@@ -1,7 +1,10 @@
 const audit = require('express').Router();
-const { storeCCTVSpotAudit } = require('./storeAudit');
+const { storeCCTVSpotAudit, calculateWeeklyMerit } = require('./audits.js');
 
 // /odoo/audit/store_cctv_audit
 audit.post('/store_cctv_audit', storeCCTVSpotAudit);
+
+// /odoo/audit/calculate_weekly_merit
+audit.post('/calculate_weekly_merit', calculateWeeklyMerit);
 
 module.exports = audit;
