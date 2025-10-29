9e21db03bd4497255e9ef8d8732d71bd1b33dce5
henryjre <pinedahenryjre@gmail.com>
2025-10-29 17:03:56 +0800

    Refactor test.js to improve code consistency and readability. Standardize string quotes, streamline command data structure, and enhance error handling in the interaction response. This update enhances maintainability and clarity in the command execution flow.

--- src/backup/commands/test.js
+++ src/backup/commands/test.js
@@ -2,30 +2,28 @@ const {
   SlashCommandBuilder,
   MessageFlags,
   AttachmentBuilder,
-  EmbedBuilder,
-} = require("discord.js");
-const fs = require("fs");
-const { table } = require("table");
+  EmbedBuilder
+} = require('discord.js');
+const fs = require('fs');
+const { table } = require('table');
 
-const { odooLogin, jsonRpc } = require("../../odooRpc.js");
+const { odooLogin, jsonRpc } = require('../../odooRpc.js');
 
 const dbName = process.env.odoo_db;
 const password = process.env.odoo_password;
 
 module.exports = {
   cooldown: 10,
-  data: new SlashCommandBuilder()
-    .setName("ping")
-    .setDescription("Testing purposes!"),
+  data: new SlashCommandBuilder().setName('ping').setDescription('Testing purposes!'),
   async execute(interaction, client) {
     const message = await interaction.deferReply({
-      flags: MessageFlags.Ephemeral,
+      flags: MessageFlags.Ephemeral
     });
 
-    if (!interaction.user.id === "748568303219245117") {
+    if (!interaction.user.id === '748568303219245117') {
       await interaction.reply({
         content: `🔴 ERROR: This command can only be used by Waffle Man.`,
-        flags: MessageFlags.Ephemeral,
+        flags: MessageFlags.Ephemeral
       });
       return;
     }
@@ -43,31 +41,31 @@ module.exports = {
     // fs.writeFileSync(filePath, formattedJson);
 
     const data = [
-      ["Product Name", "Quantity", "Unit"],
-      ["PRD - Famous Brownie Batter", "100", "g"],
-      ["SPK1 - Cheese Spread", "-500", "g"],
-      ["SPK1 - Chocolate", "900", "g"],
+      ['Product Name', 'Quantity', 'Unit'],
+      ['PRD - Famous Brownie Batter', '100', 'g'],
+      ['SPK1 - Cheese Spread', '-500', 'g'],
+      ['SPK1 - Chocolate', '900', 'g']
     ];
 
     // Table options
     const config = {
       columns: {
-        0: { alignment: "center", width: 30 },
-        1: { alignment: "center", width: 10 },
-        2: { alignment: "center", width: 5 },
-      },
+        0: { alignment: 'center', width: 30 },
+        1: { alignment: 'center', width: 10 },
+        2: { alignment: 'center', width: 5 }
+      }
     };
 
-    const tableString = "```" + table(data, config) + "```";
+    const tableString = '```' + table(data, config) + '```';
 
     const embed = new EmbedBuilder()
-      .setTitle("Inventory Table")
+      .setTitle('Inventory Table')
       .setDescription(tableString)
       .setColor(0x00ae86);
 
     // Send the JSON file as an attachment
     await interaction.channel.send({
-      embeds: [embed],
+      embeds: [embed]
     });
 
     const newMessage = `API Latency: ${client.ws.ping}\nClient Ping: ${
@@ -75,13 +73,13 @@ module.exports = {
     }`;
 
     await interaction.channel.send({
-      content: newMessage,
+      content: newMessage
     });
 
     await interaction.editReply({
-      content: "Success!",
+      content: 'Success!'
     });
-  },
+  }
 };
 
 async function odooFunc() {
@@ -89,21 +87,21 @@ async function odooFunc() {
 
   try {
     const params = {
-      model: "product.template",
-      method: "search_read",
+      model: 'product.template',
+      method: 'search_read',
       domain: [
-        ["categ_id", "in", [31, 9, 7]],
+        ['categ_id', 'in', [31, 9, 7]]
         // ["company_id", "in", [branch.cid]],
       ],
-      fields: ["name", "uom_name", "id"], //discount mode: per_order, per_point, percent
+      fields: ['name', 'uom_name', 'id'], //discount mode: per_order, per_point, percent
       offset: null,
-      limit: null,
+      limit: null
       //   order: "date asc",
     };
 
-    const request = await jsonRpc("call", {
-      service: "object",
-      method: "execute",
+    const request = await jsonRpc('call', {
+      service: 'object',
+      method: 'execute',
       args: [
         dbName,
         2,
@@ -113,22 +111,22 @@ async function odooFunc() {
         params.domain,
         params.fields,
         params.offset,
-        params.limit,
+        params.limit
         // params.order,
-      ],
+      ]
     });
 
     if (request.error) {
-      throw new Error("rpc_error");
+      throw new Error('rpc_error');
     }
 
     if (!request.result?.length) {
-      throw new Error("no_data_found");
+      throw new Error('no_data_found');
     }
 
-    return { ok: true, message: "success", data: request.result };
+    return { ok: true, message: 'success', data: request.result };
   } catch (error) {
-    console.error("Error:", error);
+    console.error('Error:', error);
     return { ok: false, message: error.message, data: [] };
   }
 }
