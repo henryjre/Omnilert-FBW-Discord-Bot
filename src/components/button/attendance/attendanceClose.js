6c6f498fc3b2aa456f4af244e1ec7bc81ba58885
henryjre <pinedahenryjre@gmail.com>
2025-11-05 22:47:28 +0800

    Add attendanceClose button to handle audit completion confirmation. This new feature includes a modal with input fields for confirmation and message ID, ensuring only users with the management role can access it, thereby enhancing user interaction and security in the attendance process.

---
+++ src/components/button/attendance/attendanceClose.js
@@ -0,0 +1,51 @@
+const {
+  ActionRowBuilder,
+  MessageFlags,
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle
+} = require('discord.js');
+
+const managementRole = '1314413671245676685';
+
+module.exports = {
+  data: {
+    name: `attendanceClose`
+  },
+  async execute(interaction, client) {
+    if (!interaction.member.roles.cache.has(managementRole)) {
+      return await interaction.reply({
+        content: `🔴 ERROR: You cannot use this button.`,
+        flags: MessageFlags.Ephemeral
+      });
+    }
+
+    const modal = new ModalBuilder()
+      .setCustomId('posAuditCompleteConfirmation')
+      .setTitle('CONFIRMATION');
+
+    const firstInput = new TextInputBuilder()
+      .setCustomId('posAuditCompleteInput')
+      .setLabel(`Confirm audit completion?`)
+      .setStyle(TextInputStyle.Short)
+      .setPlaceholder("Type 'confirm' to confirm audit complete.")
+      .setValue('confirm')
+      .setMaxLength(7)
+      .setRequired(true);
+
+    const secondInput = new TextInputBuilder()
+      .setCustomId('posSessionMessageId')
+      .setLabel(`Message ID (DO NOT CHANGE)`)
+      .setStyle(TextInputStyle.Short)
+      .setValue(interaction.message.id)
+      .setMaxLength(100)
+      .setRequired(true);
+
+    const firstActionRow = new ActionRowBuilder().addComponents(firstInput);
+    const secondActionRow = new ActionRowBuilder().addComponents(secondInput);
+
+    modal.addComponents(firstActionRow, secondActionRow);
+
+    await interaction.showModal(modal);
+  }
+};
