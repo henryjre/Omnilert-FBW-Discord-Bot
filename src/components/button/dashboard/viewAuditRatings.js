6b35822b41b926551b1fa470f7110f96d4490b72
henryjre <pinedahenryjre@gmail.com>
2025-11-06 17:14:15 +0800

    Update viewAuditRatings button logic: modify user validation to check against the slash command user instead of the embed field. This enhances security by ensuring only the intended user can interact with the button.

--- src/components/button/dashboard/viewAuditRatings.js
+++ src/components/button/dashboard/viewAuditRatings.js
@@ -22,11 +22,13 @@ module.exports = {
     const allEmbeds = interaction.message.embeds;
     const messageEmbed = allEmbeds[0];
 
-    const employeeField = messageEmbed.data.fields.find((field) => field.name === 'Employee');
-
-    if (!employeeField.value.includes(interaction.user.id)) {
-      replyEmbed.setDescription(`🔴 ERROR: You cannot use this button.`).setColor('Red');
-      return await interaction.reply({ embeds: [replyEmbed], flags: MessageFlags.Ephemeral });
+    const slashInteraction = interaction.message.interaction;
+    if (slashInteraction) {
+      const slashUser = slashInteraction.user;
+      if (!slashUser.id.includes(interaction.user.id)) {
+        replyEmbed.setDescription(`🔴 ERROR: You cannot use this button.`).setColor('Red');
+        return await interaction.reply({ embeds: [replyEmbed], flags: MessageFlags.Ephemeral });
+      }
     }
 
     const preloadEmbed = new EmbedBuilder()
@@ -55,8 +57,7 @@ module.exports = {
       .setDescription(`## 📈 AUDIT RATINGS\n\u200b\n${tableStr}`)
       .setFooter({
         text: `Page ${page} of ${totalPages} | Showing ${start + 1} - ${end} of ${total} entries`
-      })
-      .setFields({ name: 'Employee', value: employeeField.value });
+      });
 
     const nextButton = new ButtonBuilder()
       .setCustomId('nextAuditRatings')
